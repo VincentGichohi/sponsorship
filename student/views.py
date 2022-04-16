0495774e6e4a19ce8304d874272a6ce53ebb930a
VincentGichohi <vincentegichohi@gmail.com>
2022-04-16 15:39:32 +0300

    Added a base.html page

--- student/views.py
+++ student/views.py
@@ -4,3 +4,8 @@ from django.views.generic.base import TemplateView
 class Homepage(TemplateView):
     template_name = 'index.html'
 
+class BaseTemplateView(TemplateView):
+    template_name = 'base.html'
+
+class Demo(TemplateView):
+    template_name = 'student/student_register.html'
