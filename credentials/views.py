0e66afe5406a824de8b9dc0beb9c3f6d377c679c
VincentGichohi <vincentegichohi@gmail.com>
2022-05-13 08:51:11 +0300

    Add a Signup view

--- credentials/views.py
+++ credentials/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from credentials.forms import RegisterForm
+from django.views.generic.edit import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+from credentials.models import MyUser
+from django.urls import reverse, reverse_lazy
 
-# Create your views here.
+class SignupView(SuccessMessageMixin, CreateView):
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
+    form_class = RegisterForm
+    success_message = "Your account has been created successfully."
