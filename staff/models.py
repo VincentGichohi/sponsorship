041e54aa2517e405567ec25094d8d2fa66bd1bb0
VincentGichohi <vincentegichohi@gmail.com>
2022-08-29 22:55:42 +0300

    add a foreign key associated to the user

--- staff/models.py
+++ staff/models.py
@@ -1,2 +1,8 @@
 from django.db import models
+
+import credentials.models
 from credentials.models import *
+
+
+class Staff(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='user')
