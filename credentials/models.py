6bf2476253e9a711174eec4f12924f0605b4695c
VincentGichohi <vincentegichohi@gmail.com>
2022-08-29 22:36:26 +0300

    add created at field to myuser model

--- credentials/models.py
+++ credentials/models.py
@@ -61,6 +61,7 @@ class MyUser(AbstractBaseUser, PermissionsMixin):
     is_sponsor = models.BooleanField(default=False)
     staff = models.BooleanField(default=False)
     admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
