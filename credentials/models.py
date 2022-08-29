9c4ed128a8bb70e1512a7d77648ac1c37a31f45e
VincentGichohi <vincentegichohi@gmail.com>
2022-08-29 22:36:58 +0300

    add updated at to custom user model

--- credentials/models.py
+++ credentials/models.py
@@ -62,7 +62,7 @@ class MyUser(AbstractBaseUser, PermissionsMixin):
     staff = models.BooleanField(default=False)
     admin = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    updated_at = models.DateTimeField(auto_now=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
