c6029603bc7594b416a19d165f983aa9ac9eec96
VincentGichohi <vincentegichohi@gmail.com>
2022-08-30 11:44:26 +0300

    modify updated at model type

--- credentials/models.py
+++ credentials/models.py
@@ -63,7 +63,7 @@ class MyUser(AbstractBaseUser, PermissionsMixin):
     staff = models.BooleanField(default=False)
     admin = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True, default=now, editable=False)
-    updated_at = models.DateTimeField(auto_now=True, editable=True)
+    updated_at = models.DateTimeField(auto_now_add=True, editable=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
