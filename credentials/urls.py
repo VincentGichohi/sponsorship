76b89bcd4ca4c10340ef111fe8544a9de2df6bc7
VincentGichohi <vincentegichohi@gmail.com>
2022-08-30 12:44:37 +0300

    add a template name to signupview url

--- credentials/urls.py
+++ credentials/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from credentials.views import *
 
 urlpatterns = [
-    path('', SignupView.as_view()),
+    path('', SignupView.as_view(template_name='student_register.html')),
 ]
