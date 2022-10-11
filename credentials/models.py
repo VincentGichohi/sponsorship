1afce50d72381d4753f0de96ce8329956272fa81
VincentGichohi <vincentegichohi@gmail.com>
2022-10-11 09:45:05 +0300

    save user profiles

--- credentials/models.py
+++ credentials/models.py
@@ -65,3 +65,7 @@ def create_user_profile(sender, instance, **kwargs):
             Staff.objects.create(admin=instance)
         if instance.user_type == 3:
             Student.objects.create(admin=instance)
+
+
+@receiver(post_save, sender=CustomUser)
+def save_user_profile(sender, instance, **kwargs):
