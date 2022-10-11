7cb0d7ae643b03161dc97154c21d77e85cb19ab1
VincentGichohi <vincentegichohi@gmail.com>
2022-10-11 12:27:50 +0300

    add formsettings class to forms

--- credentials/forms.py
+++ credentials/forms.py
@@ -2,3 +2,11 @@ from django import forms
 from django.forms.widgets import DateInput, TextInput
 from .models import *
 
+
+class FormSettings(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(FormSettings, self).__init__(*args, **kwargs)
+        # Making changes
+        for field in self.visible_fields():
+            field.field.widget.attrs['class'] = 'form-control'
+
