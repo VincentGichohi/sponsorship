d204eb903c895ccebfa3d99f6036b26d33e566e0
VincentGichohi <vincentegichohi@gmail.com>
2022-10-11 09:56:17 +0300

    add course model and return statements

--- staff/models.py
+++ staff/models.py
@@ -2,7 +2,19 @@ from django.db import models
 from credentials.models import CustomUser
 
 
+class Course(models.Model):
+    name = models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Staff(models.Model):
     course = models.ForeignKey(Course, on_delete=models.DO_NOTHING, null=True, blank=True)
     admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.admin.last_name + " " + self.admin.first_name
+
