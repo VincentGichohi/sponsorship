fc295aa16d197f818eefe2fce6e9399972a88e4a
VincentGichohi <vincentegichohi@gmail.com>
2022-10-11 13:08:52 +0300

    add a clean email method

--- credentials/forms.py
+++ credentials/forms.py
@@ -11,7 +11,7 @@ class FormSettings(forms.ModelForm):
             field.field.widget.attrs['class'] = 'form-control'
 
 
-class CustomerUserForm(FormSettings):
+class CustomUserForm(FormSettings):
     email = forms.EmailField(required=True)
     gender = forms.ChoiceField(choices=[('M', 'Male'), ('F', 'Female')])
     first_name = forms.CharField(required=True)
@@ -24,10 +24,28 @@ class CustomerUserForm(FormSettings):
     profile_pic = forms.ImageField()
 
     def __init__(self, *args, **kwargs):
-        super(CustomerUserForm, self).__init__(*args, **kwargs)
+        super(CustomUserForm, self).__init__(*args, **kwargs)
 
         if kwargs.get('instance'):
             instance = kwargs.get('instance').admin.__dict__
             self.fields['password'].required = False
-            for field in CustomerUserForm.Meta.fields:
+            for field in CustomUserForm.Meta.fields:
                 self.fields[field].initial = instance.get(field)
+            if self.instance.pk is not None:
+                self.fields['password'].widget.attrs['placeholder'] = 'Fill this only if you wish to update password'
+
+    def clean_email(self, *args, **kwargs):
+        formEmail = self.cleaned_data['email'].lower()
+        if self.instance.pk is None:
+            if CustomUser.objects.filter(email=formEmail).exists():
+                raise forms.ValidationError(
+                    "The given email is already registered"
+                )
+        else:
+            dbEmail = self.Meta.model.objects.get(
+                id=self.instance.pk).admin.email.lower()
+            if dbEmail != formEmail:
+                if CustomUser.objects.filter(email=formEmail).exists():
+                    raise forms.ValidationError("The given email is already registered")
+        return formEmail
+
