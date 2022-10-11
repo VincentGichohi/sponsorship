5862c796fef59330c604b96d369f0df12393a38d
VincentGichohi <vincentegichohi@gmail.com>
2022-10-11 09:14:13 +0300

    add imports to models

--- credentials/models.py
+++ credentials/models.py
@@ -1,1 +1,5 @@
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.db import models
