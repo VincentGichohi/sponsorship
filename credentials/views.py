c5b71e757274a645144918471b03e6cb5d74ed6c
VincentGichohi <vincentegichohi@gmail.com>
2022-10-11 12:16:52 +0300

    edit views

--- credentials/views.py
+++ credentials/views.py
@@ -1,15 +1,1 @@
-from django.shortcuts import render
-from credentials.forms import RegisterForm
-from django.views.generic.edit import CreateView
-from django.contrib.messages.views import SuccessMessageMixin
-from credentials.models import MyUser
-from django.urls import reverse, reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView
-
-class SignupView(SuccessMessageMixin, CreateView):
-    template_name = 'student/student_register.html'
-    success_url = reverse_lazy('student/login')
-    form_class = RegisterForm
-    success_message = "Your account has been created successfully."
 
