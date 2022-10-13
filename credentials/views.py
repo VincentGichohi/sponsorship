48d7fe3c8c353a55dcce3b06dbfc94210ba7ef83
VincentGichohi <vincentegichohi@gmail.com>
2022-10-13 12:59:20 +0300

    add home view

--- credentials/views.py
+++ credentials/views.py
@@ -6,3 +6,10 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.conf import settings
 from django.core.mail import send_mail
+
+
+def home_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request, 'school/index.html')  # subject to change
+
