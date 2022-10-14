6c4ec312ed589685e750f4b7de4ec04935d0a615
VincentGichohi <vincentegichohi@gmail.com>
2022-10-14 23:45:45 +0300

    add fields to student model

--- student/models.py
+++ student/models.py
@@ -3,3 +3,8 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=200)
+    address = models.TextField()
+    phone_number = models.TextField()
+    email = models.EmailField(unique=True)
+    birth_certificate = models.FileField(upload_to='images')
+    national_id_file = models.FileField(upload_to='images')
