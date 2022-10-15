c97f3d13e1f80b1206c3aec9d47dcbc3f996713f
VincentGichohi <vincentegichohi@gmail.com>
2022-10-15 23:57:15 +0300

    import staff models to credentials

--- credentials/models.py
+++ credentials/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, Permis
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.db import models
+from staff.models import *
 
 
 class CustomUserManager(BaseUserManager):
