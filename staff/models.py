2395ef19fa54f9bc73486a77273ecae4f9764ad4
VincentGichohi <vincentegichohi@gmail.com>
2022-10-15 23:46:39 +0300

    add staff model

--- staff/models.py
+++ staff/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
 
+class Staff(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.admin.last_name + " " + self.admin.first_name
+
+
 class LeaveReportStaff(models.Model):
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
