5c8e1c63f0393a17a94a2e7538b9d7e4d0046440
VincentGichohi <vincentegichohi@gmail.com>
2022-10-16 22:18:40 +0300

    add class meta to sessionform

--- credentials/forms.py
+++ credentials/forms.py
@@ -64,3 +64,17 @@ class AdminForm(CustomUserForm):
         fields = CustomUserForm.Meta.fields
 
 
+
+class SessionForm(FormSettings):
+    def __init__(self, *args, **kwargs):
+        super(SessionForm, self).__init__(*args, **kwargs)
+    
+    class Meta:
+        model = Session
+        fields = "__all__"
+        widgets = {
+            'start_year': DateInput(attrs={'type': 'date'}),
+            'end_year': DateInput(attrs={'type': 'date'}),
+        }
+
+    
