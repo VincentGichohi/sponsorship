62b72e2583325642266c9ad94d2a0bbf506c29d5
VincentGichohi <vincentegichohi@gmail.com>
2022-10-16 18:02:13 +0300

    add staffform with class meta

--- staff/forms.py
+++ staff/forms.py
@@ -1,3 +1,15 @@
 from django import forms
+
+from credentials.forms import CustomUserForm
 from . import models
 from credentials.models import *
+
+
+class StaffForm(CustomUserForm):
+    def __init__(self, *args, **kwargs):
+        super(StaffForm, self).__init__(*args, **kwargs)
+
+    class Meta(CustomUserForm.Meta):
+        model = Staff
+        fields = CustomUserForm.Meta.fields + ['course']
+        
