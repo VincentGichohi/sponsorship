c098bcf07bc2999039bb0417047be2303366a1a0
VincentGichohi <vincentegichohi@gmail.com>
2022-10-16 21:54:55 +0300

    add courseform and initialize

--- staff/forms.py
+++ staff/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from credentials.forms import CustomUserForm
 from . import models
 from credentials.models import *
+from credentials.forms import *
 
 
 class StaffForm(CustomUserForm):
@@ -12,4 +13,10 @@ class StaffForm(CustomUserForm):
     class Meta(CustomUserForm.Meta):
         model = Staff
         fields = CustomUserForm.Meta.fields + ['course']
+
+
+
+class CourseForm(FormSettings):
+    def __init__(self, *args, **kwargs):
+        super(CourseForm, self).__init__(*args, **kwargs)
         
