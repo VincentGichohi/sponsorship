2531eea7e3daf7cc52d4d892c353cda6ec37ec7c
VincentGichohi <vincentegichohi@gmail.com>
2022-10-16 17:50:40 +0300

    add studentform

--- student/forms.py
+++ student/forms.py
@@ -1,2 +1,11 @@
 from django import forms
 from . import models
+from credentials.forms import *
+
+
+class StudentForm(CustomUserForm):
+    def __init__(self, *args, **kwargs):
+        super(StudentForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        
