52ac95346d83783a2f7105ef3342ecb5ccf06dfc
VincentGichohi <vincentegichohi@gmail.com>
2022-10-18 15:54:55 +0300

    add imports to credentials views

--- credentials/views.py
+++ credentials/views.py
@@ -1,1 +1,5 @@
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .email_backend import EmailBackend
+from django.contrib import messages
+from .forms import CustomUserForm
+from django.contrib.auth import login, logout
