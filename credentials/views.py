a534f6d45118377009e88121285103c218e2c6ad
VincentGichohi <vincentegichohi@gmail.com>
2022-10-18 15:56:43 +0300

    add a register view

--- credentials/views.py
+++ credentials/views.py
@@ -3,3 +3,13 @@ from .email_backend import EmailBackend
 from django.contrib import messages
 from .forms import CustomUserForm
 from django.contrib.auth import login, logout
+
+
+def account_register(request):
+    userForm = CustomUserForm(request.POST or None)
+    context = {
+        'userForm': userForm,
+    }
+    if request.method == 'POST':
+        if userForm.is_valid():
+
