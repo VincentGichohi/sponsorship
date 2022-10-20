bcd67dd8bd7cb62ec430da1a7bba1dd6a6c9bc5d
VincentGichohi <vincentegichohi@gmail.com>
2022-10-20 22:52:16 +0300

    add a class accountmiddlewaremixin

--- credentials/middleware.py
+++ credentials/middleware.py
@@ -2,3 +2,7 @@ from django.utils.deprecation import MiddlewareMixin
 from django.urls import reverse
 from django.shortcuts import redirect
 from django.contrib import messages
+
+
+class AccountCheckMiddleware(MiddlewareMixin):
+    def
