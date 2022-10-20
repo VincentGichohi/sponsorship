2a879305614cb57df67047a3a1d9006c416e157d
VincentGichohi <vincentegichohi@gmail.com>
2022-10-20 22:54:52 +0300

    add a modulename demo

--- credentials/middleware.py
+++ credentials/middleware.py
@@ -6,3 +6,6 @@ from django.contrib import messages
 
 class AccountCheckMiddleware(MiddlewareMixin):
     def process_view(self, request, view_func, view_args, view_kwargs):
+        modulename = view_func.__module__
+        user = request.user  # Who is the current user?
+
