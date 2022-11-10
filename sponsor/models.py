cbf7ce9dd16773beeb79a90a54c7a857e2c3e581
VincentGichohi <vincentegichohi@gmail.com>
2022-11-10 12:48:15 +0300

    add fields to sponsorship model

--- sponsor/models.py
+++ sponsor/models.py
@@ -2,5 +2,19 @@ from django.db import models
 from credentials.models import CustomUser
 
 
+GENDER = [
+    ('M', "Male"),
+    ('F', "Female")
+]
+
 class Sponsor(models.Model):
-    
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    gender = models.CharField(choices=GENDER, max_length=1)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
