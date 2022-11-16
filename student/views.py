614eea6fe8d50d067326ca92000c374fd647b313
VincentGichohi <vincentegichohi@gmail.com>
2022-11-16 21:33:29 +0300

    add student home view

--- student/views.py
+++ student/views.py
@@ -11,3 +11,18 @@ from django.shortcuts import (HttpResponseRedirect, get_object_or_404,
                               redirect, render)
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
+
+
+def student_home(request):
+    student = get_object_or_404(Student, admin=request.user)
+    total_subject = Subject.objects.filter(course=student.course).count()
+    total_attendance = AttendanceReport.objects.filter(student=student).count()
+    total_present = AttendanceReport.objects.filter(student=student, status=True).count()
+    if total_attendance == 0:
+        percent_absent = percent_present = 0
+    else:
+        percent_present = math.floor((total_present/total_attendance) * 100)
+        percent_absent  = math.ceil(100 - percent_present)
+
+    subject_name = []
+    
