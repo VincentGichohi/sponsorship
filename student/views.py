8b998ee179c0cb2f24ec01c2ccb0cfc15a86cbf4
VincentGichohi <vincentegichohi@gmail.com>
2022-11-16 21:27:59 +0300

    add imports to student views

--- student/views.py
+++ student/views.py
@@ -1,1 +1,13 @@
 from django.shortcuts import render
+from .models import *
+from .forms import *
+import json
+import math
+from datetime import datetime
+from django.contrib import messages
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import (HttpResponseRedirect, get_object_or_404,
+                              redirect, render)
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
