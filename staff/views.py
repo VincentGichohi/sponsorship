08db229100532bc00b591313d51db14ecd2b9316
VincentGichohi <vincentegichohi@gmail.com>
2022-11-18 20:48:44 +0300

    add a view for staff to view home

--- staff/views.py
+++ staff/views.py
@@ -13,3 +13,27 @@ from credentials.forms import *
 from student.forms import *
 
 
+def staff_home(request):
+    staff = get_object_or_404(Staff, admin=request.user)
+    total_students = Student.objects.filter(course=staff.course).count()
+    total_leave = LeaveReportStaff.objects.filter(staff=staff).count()
+    subjects = Subject.objects.filter(staff=staff)
+    total_subject = subjects.count()
+    attendance_list = Attendance.objects.filter(subject__in=subjects)
+    total_attendance = attendance_list.count()
+    attendance_list = []
+    subject_list = []
+    for subject in subjects:
+        attendance_count = Attendance.objects.filter(subject=subject).count()
+        subject_list.append(subject.name)
+        attendance_list.append(attendance_count)
+    context = {
+        'page_title': 'Staff panel - ' + str(staff.admin.last_name) + '(' + str(staff.course) + ')',
+        'total_students': total_students,
+        'total_attendance': total_leave,
+        'total_subject': total_subject,
+        'subject_list': subject_list,
+        'attendance_list': attendance_list
+    }
+    return render(request, 'staff_template/home_content.html', context)
+
