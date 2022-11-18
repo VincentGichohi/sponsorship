9181ebbb462b982384c58852ab12204a107d16c6
VincentGichohi <vincentegichohi@gmail.com>
2022-11-18 16:08:53 +0300

    add view for viewing student profile and updating it

--- student/views.py
+++ student/views.py
@@ -137,5 +137,31 @@ def student_view_profile(request):
     student = get_object_or_404(Student, admin=request.user)
     form = StudentEditForm(request.POST or None, request.FILES or None, instance=student)
     context = {
-        
+        'form': form,
+        'page_title': "View/Edit Profile"
     }
+    if request.method == 'POST':
+        try:
+            if form.is_valid():
+                first_name = form.cleaned_data.get('first_name')
+                last_name = form.cleaned_data.get('last_name')
+                password = form.cleaned_data.get('password') or None
+                address = form.cleaned_data.get('address')
+                gender = form.cleaned_data.get('gender')
+                passport = request.FILES.get('profile_pic') or None
+                admin = student.admin
+                if password != None:
+                    admin.set_password(password)
+                if passport != None:
+                    fs = FileSystemStorage()
+                    filename = fs.save(passport.name, passport)
+                    passport_url = fs.url(filename)
+                    admin.profile_pic = passport_url
+                admin.first_name = first_name
+                admin.last_name = last_name
+                admin.address = address
+                admin.gender = gender
+                admin.save()
+                messages.success(request, 'Profile Updated')
+                return redirect(reverse('student_view_profile'))
+                
