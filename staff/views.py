ead65a72ed63728adb51d21feba56e2105846b75
VincentGichohi <vincentegichohi@gmail.com>
2022-11-18 16:26:48 +0300

    add imports to staff file

--- staff/views.py
+++ staff/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+import json
+from django.contrib import messages
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse, JsonResponse
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from .forms import *
+from .models import *
 
-# Create your views here.
